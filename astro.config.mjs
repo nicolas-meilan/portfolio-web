987b4b6bfb3a8b2eb4ffeb93ea9dda22d39767dd
nicolas-meilan <sebastian.nicolas.meilan@gmail.com>
2024-03-11 16:22:40 -0300

    Add deploy action to github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://nicolas-meilan.github.io/portfolio-web',
+  base: '/portfolio-web',
   integrations: [tailwind(), astroI18next()],
 });
